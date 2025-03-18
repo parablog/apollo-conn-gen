84f3f25c3eebb9141ecfa411761836b6919e878b
fernando-apollo <fernando@apollographql.com>
2025-03-18 16:50:48 +0000

    0.0.9

--- src/oas/index.ts
+++ src/oas/index.ts
@@ -1,26 +1,27 @@
-import {type IType } from './nodes/internal.js';
-import { Type } from './nodes/internal.js'
-import { Param } from './nodes/internal.js'
-import { Prop } from './nodes/internal.js'
-import { PropArray } from './nodes/internal.js'
-import { PropObj } from './nodes/internal.js'
-import { PropRef } from './nodes/internal.js'
-import { PropScalar } from './nodes/internal.js'
-import { ReferenceObject } from './nodes/internal.js'
-import { Arr } from './nodes/internal.js'
-import { CircularRef } from './nodes/internal.js'
-import { Composed } from './nodes/internal.js'
-import { En } from './nodes/internal.js'
-import { Factory } from './nodes/internal.js'
-import { Get } from './nodes/internal.js'
-import { Obj } from './nodes/internal.js'
-import { Ref } from './nodes/internal.js'
-import { Response } from './nodes/internal.js'
-import { Scalar } from './nodes/internal.js'
-import { Union } from './nodes/internal.js'
+import { type IType } from './nodes/internal.js';
+import { Type } from './nodes/internal.js';
+import { Param } from './nodes/internal.js';
+import { Prop } from './nodes/internal.js';
+import { PropArray } from './nodes/internal.js';
+import { PropObj } from './nodes/internal.js';
+import { PropRef } from './nodes/internal.js';
+import { PropScalar } from './nodes/internal.js';
+import { ReferenceObject } from './nodes/internal.js';
+import { Arr } from './nodes/internal.js';
+import { CircularRef } from './nodes/internal.js';
+import { Composed } from './nodes/internal.js';
+import { En } from './nodes/internal.js';
+import { Factory } from './nodes/internal.js';
+import { Get } from './nodes/internal.js';
+import { Obj } from './nodes/internal.js';
+import { Ref } from './nodes/internal.js';
+import { Response } from './nodes/internal.js';
+import { Scalar } from './nodes/internal.js';
+import { Union } from './nodes/internal.js';
 
 export { OasGen } from './oasGen.js';
-export { type IType,
+export {
+  type IType,
   Param,
   Prop,
   PropArray,
